5a92688278357e8c1601005efd32529dbe043443
seni0rDEV <132949016+seni0rDEV@users.noreply.github.com>
2024-09-03 14:11:47 +0100

    toggling issue resolved

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+isMenuOpen = true;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
